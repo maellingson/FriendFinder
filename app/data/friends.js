286af8636eabc82c53138868b7b273f030232d75
Marissa <marissaellingson@Marissas-MacBook.local>
2018-07-23 16:46:26 -0500

    Updated Friends

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var friendsData = [
+    {
+        name: "Mike Wheeler",
+        photo: "http://pm1.narvii.com/6186/7526a40089639df7bc8c5f83cd66e43b834842f8_00.jpg",
+        scores: [4,5,4,5,4,5,4,5,4,5],
+    },
+    {
+        name: "Eleven",
+        photo: "https://vignette.wikia.nocookie.net/strangerthings8338/images/1/10/Eleven_S2.png/revision/latest?cb=20171017130709",
+        scores: [4,3,4,2,2,4,4,5,1,5],
+    },
+
+    {
+        name: "Dustin Henderson",
+        photo: "https://data.whicdn.com/images/303274299/large.jpg",
+        scores: [5,5,5,5,5,5,5,5,5,5]
+    },
+
+    {
+        name: "Lucas Sinclair",
+        photo: "https://pm1.narvii.com/6739/bec914eb918f987e4122f0f7e33ab6da878a8041v2_00.jpg",
+        scores: [3,3,4,2,4,5,3,5,2,5]
+    },
+
+    {
+        name: "Will Byers",
+        photo: "https://vignette.wikia.nocookie.net/strangerthings8338/images/e/e4/Will_Byers.png/revision/latest/scale-to-width-down/310?cb=20170124225951",
+        scores: [4,3,4,4,5,5,5,2,4,5],
+    },
+
+    {
+        name: "Steve Harrington",
+        photo: "https://vignette.wikia.nocookie.net/strangerthings8338/images/7/78/Image.jpg/revision/latest?cb=20171113113237",
+        scores: [2,3,1,2,5,3,4,1,2,3],
+    },
+
+    {
+        name: "Nancy Wheeler",
+        photo: "https://vignette.wikia.nocookie.net/strangerthings8338/images/5/5d/Nancy_S2.png/revision/latest/scale-to-width-down/310?cb=20171229091940",
+        scores: [4,3,2,4,3,5,4,3,5,3],
+    },
+
+    {
+        name: "Max Mayfield",
+        photo: "https://strangerthingsttu.weebly.com/uploads/1/1/4/5/114552629/max-2.png",
+        scores: [2,1,5,2,3,4,5,2,1,3],
+    },
+]
+
+module.exports = friendsData;

2361afa2788a1dcf5df4d9b2c29918a5c17cb72c
Marissa <marissaellingson@Marissas-MacBook.local>
2018-07-23 19:31:39 -0500

    api routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,40 @@
+var friendsData = require("../data/friends.js")
+
+app.get("/api/friends", function (req, res) {
+    res.JSON(friendsData)
+});
+
+app.post("/api/friends", function (req, res) {
+    var newFriend = {
+        name = req.body.name,
+        photo = req.body.photo,
+        scores =[]
+    }
+    var scoresArray = [];
+    for (var i = 0; i < req.body.scores.length; i++) {
+        scoresArray.push(parseInt(req.body.scores[i]))
+    }
+
+    newFriend.scores = scoresArray;
+
+    var compareScores = [];
+    for (var i = 0; i < friendsData.length; i++) {
+        var activeCompare = 0;
+        for (var x = 0; x < newFriend.scores.length; x++) {
+            activeCompare += math.abs(newFriend.scores[x] - friendsData[i].scores[x])
+            compareScores.push(activeCompare);
+        }
+        var compatScores = 0;
+        for (var i = 1; i < friendsData.length; i++) {
+            if (compareScores[i] <= compareScores[compatScores]) {
+                compatScores = i;
+            }
+        }
+        var bestFriend = friendsData[compatScores];
+        res.json(bestFriend);
+        friendsData.push(newFriend);
+    }
+
+})
+
+module.exports = apiRoutes;

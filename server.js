b536a8c5b050015177a6482f4adc1fd4ff8c27a3
Marissa <marissaellingson@Marissas-MacBook.local>
2018-07-25 16:05:46 -0500

    Updated ENV Ports

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require("./app/data/friends");
 
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());

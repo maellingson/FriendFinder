dcb09319d77f3b0b93e0fbe8d97420ee361414c4
Marissa <marissaellingson@Marissas-MacBook.local>
2018-07-25 18:48:45 -0500

    pathways updated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,6 @@
+var friendsData = require("../data/friends.js");
 var express = require("express");
-
 var app = express();
-
-var friendsData = require("../data/friends");
 
 module.exports = function (app) {
     app.get("api/friends", function (req, res) {
